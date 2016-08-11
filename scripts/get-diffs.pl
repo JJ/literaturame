3b5cec637cc65831ce708d15171b9a529ce40df8
JJ Merelo <jjmerelo@gmail.com>
2016-08-11 14:16:01 +0200

    Now data on a program

--- scripts/get-diffs.pl
+++ scripts/get-diffs.pl
@@ -13,13 +13,14 @@ my $filespec = shift || die "Usage: $0 <ficheros> [git directory]\n";
 my $dir = shift || ".";
 
 my $repo = Git->repository (Directory => $dir);
+my @filespec = split(/ /,$filespec);
 
-my @revs = $repo->command('rev-list', '--all', '--', $filespec);
+my @revs = $repo->command('rev-list', '--all', '--', @filespec);
 my @data = ("Lines changed");
 
 my $prev_commit = shift @revs;
 for my $commit ( reverse @revs ) {
-  my $file_contents = $repo->command('diff','--shortstat',  "$commit..$prev_commit", $filespec );
+  my $file_contents = $repo->command('diff','--shortstat',  "$commit..$prev_commit", "--", @filespec );
   my ($insertions) = ($file_contents =~ /(\d+)\s+insertion\S+/s);
   my ($deletions) = ($file_contents =~ /(\d+)\s+deletion\S+/s);
   $insertions = $insertions || 0;
@@ -30,13 +31,13 @@ for my $commit ( reverse @revs ) {
   $prev_commit = $commit;
 }
 
-my ($last_dir_name)  = ($dir =~ m{/(\w+)/$} );
+my ($last_dir_name)  = ($dir =~ m{/([^/]+)/$} );
 my $flat_file_name = $filespec;
 
 $flat_file_name =~ s/\*/ALL/g;
 $flat_file_name =~ s/\./_/g;
 $flat_file_name =~ s/\//_/g;
-
+$flat_file_name =~ s/ /_/g;
 
 write_file("lines_$last_dir_name"."_$flat_file_name.csv", join("\n", @data));
 
