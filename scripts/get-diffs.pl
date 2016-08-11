c9065599ab0fa59ea1e99a22d1b38811c0503ba4
JJ Merelo <jjmerelo@gmail.com>
2016-08-11 13:31:26 +0200

    First version
    Which extracts the number of lines changed in a file. Very basic stuff if you are not counting words elsewhere.

---
+++ scripts/get-diffs.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.22;
+
+use JSON;
+use Git;
+use File::Slurp::Tiny qw(write_file);
+
+my $filespec = shift || die "Usage: $0 <ficheros> [git directory]\n";
+my $dir = shift || ".";
+
+my $repo = Git->repository (Directory => $dir);
+
+my @revs = $repo->command('rev-list', '--all', '--', $filespec);
+my @data = ("Lines changed");
+
+my $prev_commit = shift @revs;
+for my $commit ( reverse @revs ) {
+  my $file_contents = $repo->command('diff','--shortstat',  "$commit..$prev_commit", $filespec );
+  my ($insertions) = ($file_contents =~ /(\d+)\s+insertion\S+/s);
+  my ($deletions) = ($file_contents =~ /(\d+)\s+deletion\S+/s);
+  $insertions = $insertions || 0;
+  $deletions = $deletions || 0;
+
+  my $lines_changed = ($insertions > $deletions)?$insertions:$deletions;
+  push @data, $lines_changed;
+  $prev_commit = $commit;
+}
+
+my ($last_dir_name)  = ($dir =~ m{/(\w+)/$} );
+my $flat_file_name = $filespec;
+
+$flat_file_name =~ s/\*/ALL/g;
+$flat_file_name =~ s/\./_/g;
+$flat_file_name =~ s/\//_/g;
+
+
+write_file("lines_$last_dir_name"."_$flat_file_name", join("\n", @data));
+
