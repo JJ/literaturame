d4efd5d1abf2581263da0f275a1754468c635d9e
JJ Merelo <jjmerelo@gmail.com>
2016-08-11 19:45:16 +0200

    A script to match builds with commits
    So that we can compute the difference in words between one build and the previous one. That's the thing. We can download it with travis log, extract

---
+++ scripts/get-travis.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use v5.20;
+
+use JSON;
+use Git;
+use File::Slurp::Tiny qw(write_file);
+
+my $status = `travis show`;
+
+my ($num_builds) = ($status =~ /\s+\#(\d+)/);
+
+my %commits;
+say "Build Commit";
+for my $i (1..$num_builds) {
+  my $this_status = `travis show $i`;
+  if ( $this_status =~ /Type:\s+push/) {
+      my ($state,$commit) = ($this_status =~ /State:\s+(\w+).+\.\.\.(\w+)/s);
+      if ( $state ne 'errored' ) {
+	  say "$i $commit";
+      }
+  }
+}
+
+
+
