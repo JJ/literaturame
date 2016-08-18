78bd3e8105bdb7320c9cd2963782568612df4153
JJ Merelo <jjmerelo@gmail.com>
2016-08-18 22:38:20 +0200

    Generate reports for novels

---
+++ stats/generate-reports-novels.R
@@ -0,0 +1,32 @@
+# References for automation 
+# http://www.r-bloggers.com/how-to-source-an-r-script-automatically-on-a-mac-using-automator-and-ical/
+# http://www.engadget.com/2013/03/18/triggering-applescripts-from-calendar-alerts-in-mountain-lion/
+
+# File 1: Should be an R-Script 
+    # contains a loop that iteratively calls an Rmarkdown file (i.e. File 2)
+
+# load packages
+library(knitr)
+library(markdown)
+library(rmarkdown)
+
+# use first 5 rows of mtcars as example data
+data.files <- c( '../data/books/words-nyc-42-8.csv',
+                 '../data/books/words-granada-off.csv',
+                 '../data/books/words-hoborg.csv' )
+
+# for each file create a report
+# these reports are saved in output_dir with the name specified by output_file
+
+for (file in data.files){
+    print(file)
+    report.file <- gsub('words','report',file)
+    report.file <- gsub('csv','pdf',report.file)
+    report.file <- gsub('../data','.',report.file)
+    print(report.file)
+    rmarkdown::render(input = 'creativity-novels.Rmd', 
+                      output_format = "pdf_document",
+                      output_file = report.file,
+                      output_dir = '.')
+}
+
