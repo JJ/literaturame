f372fceb1f1eba119af138d011291f867d5b891c
JJ Merelo <jjmerelo@gmail.com>
2016-08-18 21:57:28 +0200

    Computing spectrum

--- stats/spectrum.R
+++ stats/spectrum.R
@@ -3,3 +3,6 @@ library(ggplot2)
 words <- read.csv("../data/books/words-nyc-42-8.csv")
 words.less1 <- words[-1,]
 gg.words <- ggplot(words.less1)
+
+trend <- lm( words.less1$Delta ~ words.less1$Commit )
+gg.words+geom_line(aes(x=Commit,y=Delta))
