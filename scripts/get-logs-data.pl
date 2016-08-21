761b1ec441017d7ea8cff2c2e591b1ed988db954
JJ Merelo <jjmerelo@gmail.com>
2016-08-21 19:30:37 +0200

    Adds scripts here

---
+++ scripts/get-logs-data.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use File::Slurp qw(read_file);
+use v5.14;
+
+my $dir = shift || ".";
+my @logs = glob("$dir/log*.log");
+
+my @data;
+for my $l ( @logs ) {
+  my ($number) = ( $l =~ /log-(\d+)/);
+  my $file_content = read_file ($l );
+  if ( $file_content =~ /Tests=(\d+)/ ) {
+    $data[$number] = $1;
+  }
+}
+
+
+say "Commit,Words,Delta";
+my $old_number_of_words = 0;
+for (my $i = 0; $i <=$#data; $i ++ ) {
+  if ( $data[$i] ) {
+    say "$i, $data[$i], ", $data[$i]- $old_number_of_words;
+    $old_number_of_words = $data[$i];
+  }
+}
+  
