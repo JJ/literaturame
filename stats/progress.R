973080bf19b14b1a292b75cc51d7c1a8f3d4f7a9
JJ Merelo <jjmerelo@gmail.com>
2016-08-21 18:10:02 +0200

    Adds hashslash

--- stats/progress.R
+++ stats/progress.R
@@ -4,13 +4,16 @@ library(dplyr)
 words.granada <- read.csv('../data/books/words-granada-off.csv')
 words.nyc <- read.csv('../data/books/words-nyc-42-8.csv')
 words.hoborg <- read.csv('../data/books/words-hoborg.csv')
+words.hsh <- read.csv('../data/books/words-hashlash.csv')
 
 words.granada$rel <- words.granada$Words/last(words.granada$Words)
 words.nyc$rel <- words.nyc$Words/last(words.nyc$Words)
 words.hoborg$rel <- words.hoborg$Words/last(words.hoborg$Words)
+words.hsh$rel <- words.hsh$Words/last(words.hsh$Words)
 
 words.granada$idx <- words.granada$Commit/last(words.granada$Commit)
 words.nyc$idx <- words.nyc$Commit/last(words.nyc$Commit)
 words.hoborg$idx <- words.hoborg$Commit/last(words.hoborg$Commit)
+words.hsh$idx <- words.hsh$Commit/last(words.hsh$Commit)
 
-ggplot()+geom_line(data=words.granada,aes(x=idx,y=rel,col='Granada On'))+geom_line(data=words.nyc,aes(x=idx,y=rel,col='Esquina'))+geom_line(data=words.hoborg,aes(x=idx,y=rel,col='Manuel'))
+ggplot()+geom_line(data=words.granada,aes(x=idx,y=rel,col='Granada On'))+geom_line(data=words.nyc,aes(x=idx,y=rel,col='Esquina'))+geom_line(data=words.hoborg,aes(x=idx,y=rel,col='Manuel'))+geom_line(data=words.hsh,aes(x=idx,y=rel,col='#Slash'))
