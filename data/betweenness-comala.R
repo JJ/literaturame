b26f468a842c678d60c0dd97dd9061903db4abef
JJ Merelo <jjmerelo@gmail.com>
2017-08-06 10:09:21 +0200

    Añade script

---
+++ data/betweenness-comala.R
@@ -0,0 +1,12 @@
+library(igraph)
+library(ggplot2)
+library(ggthemes)
+
+data = read.csv("red-pedro-páramo.csv", header=TRUE)
+comala.matrix <- as.matrix(data)
+g = graph.edgelist(comala.matrix,directed=F)
+bet.comala.df = data.frame(betweenness(g))
+bet.comala.df$personajes = rownames(bet.comala.df)
+
+ggplot()+geom_point(data=bet.comala.df,aes(x=reorder(personajes,-betweenness.g.),y=betweenness.g.))+scale_y_log10()+theme_tufte()+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
